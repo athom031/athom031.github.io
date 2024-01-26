f6b40c2c6f91d5c3bd814255a85754d5d21baecb
Alex Thomas <thomaalex@meta.com>
2024-01-26 08:58:21 -0800

    make hamburger icon in extended header clickable

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,4 +1,10 @@
 $(document).ready(function () {
+    // change make hamburger icon clickable if media screen is a phone
+    $('#menu').click(function () {
+        $(this).toggleClass('fa-times');
+        $('.navbar').toggleClass('nav-toggle');
+    });
+
     // <!-- attach hover logo to logo in navbar -->
     var logoLink = $('#logo-link');
     var logoImg = $('#logo-img');
--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
 $(document).ready(function () {
 
-    $('#menu').click(function () {
-        $(this).toggleClass('fa-times');
-        $('.navbar').toggleClass('nav-toggle');
-    });
-
     $(window).on('scroll load', function () {
         $('#menu').removeClass('fa-times');
         $('.navbar').removeClass('nav-toggle');
