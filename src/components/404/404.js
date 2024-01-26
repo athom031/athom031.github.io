6d20e698edff79d25ff737209be16f5ebb22f88e
Alex Thomas <thomaalex@meta.com>
2024-01-26 09:19:34 -0800

    create basic styling for all web components on website

--- src/components/404/404.js
+++ src/components/404/404.js
@@ -1,10 +1,4 @@
 $(document).ready(function(){
-
-    $('#menu').click(function(){
-        $(this).toggleClass('fa-times');
-        $('.navbar').toggleClass('nav-toggle');
-    });
-
     // <!-- load header at #header-->
     $(function(){
         $("#header").load("src/components/header/header.html");
