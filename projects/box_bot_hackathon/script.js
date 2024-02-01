6a41b0e8062ac8e546f45fddaab12cb597bb606b
Alex Thomas <thomaalex@meta.com>
2024-02-01 11:42:09 -0800

    fix link to header and footer script

--- projects/box_bot_hackathon/script.js
+++ projects/box_bot_hackathon/script.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
     // <!-- load header at #header-->
     $(function(){
-        $("#header").load("src/components/header/header.html");
+        $("#header").load("../../src/components/header/header.html");
     });
 
     // <!-- load footer at #footer-->
     $(function(){
-        $("#footer").load("src/components/footer/footer.html");
+        $("#footer").load("../../src/components/footer/footer.html");
     });
 
     // get rid of active class on page load
