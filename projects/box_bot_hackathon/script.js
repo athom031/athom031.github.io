d47d2621342360c83dea67982979cd5f6c05ae5a
Alex Thomas <thomaalex@meta.com>
2024-02-01 11:00:26 -0800

    start adding skeleton code to add project htmls

---
+++ projects/box_bot_hackathon/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    // <!-- load header at #header-->
+    $(function(){
+        $("#header").load("src/components/header/header.html");
+    });
+
+    // <!-- load footer at #footer-->
+    $(function(){
+        $("#footer").load("src/components/footer/footer.html");
+    });
+
+    // get rid of active class on page load
+    $(document).ready(function() {
+        $('.active').removeClass('active');
+    });
+});
