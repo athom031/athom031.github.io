2bdd261c17250ec399769ad222d1387e93906c7d
Alex Thomas <thomaalex@meta.com>
2024-02-02 15:43:29 -0800

    start opening up option to view all projecst

--- src/components/projects/script.js
+++ src/components/projects/script.js
@@ -1,21 +1,17 @@
-// not currently being used
+// script for ALL project page
 
 $(document).ready(function () {
-
-    $('#menu').click(function () {
-        $(this).toggleClass('fa-times');
-        $('.navbar').toggleClass('nav-toggle');
+    // <!-- load header at #header-->
+    $(function(){
+        $("#header").load("../../header/header.html");
     });
-
-    $(window).on('scroll load', function () {
-        $('#menu').removeClass('fa-times');
-        $('.navbar').removeClass('nav-toggle');
-
-        if (window.scrollY > 60) {
-            document.querySelector('#scroll-top').classList.add('active');
-        } else {
-            document.querySelector('#scroll-top').classList.remove('active');
-        }
+    // <!-- load footer at #footer-->
+    $(function(){
+        $("#footer").load("../../footer/footer.html");
+    });
+    // get rid of active class on page load
+    $(document).ready(function() {
+        $('.active').removeClass('active');
     });
 });
 
@@ -53,23 +49,6 @@ function showProjects(projects) {
     </div>`
     });
     projectsContainer.innerHTML = projectsHTML;
-
-    // vanilla tilt.js
-    // VanillaTilt.init(document.querySelectorAll(".tilt"), {
-    //     max: 20,
-    // });
-    // // vanilla tilt.js
-
-    // /* ===== SCROLL REVEAL ANIMATION ===== */
-    // const srtop = ScrollReveal({
-    //     origin: 'bottom',
-    //     distance: '80px',
-    //     duration: 1000,
-    //     reset: true
-    // });
-
-    // /* SCROLL PROJECTS */
-    // srtop.reveal('.work .box', { interval: 200 });
 
     // isotope filter products
     var $grid = $('.box-container').isotope({
